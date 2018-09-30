d1bed52c1dc951e3fa5875b2c100525febbd6864
NguyenKhanhIE3 <lord251200@gmail.com>
2018-09-30 19:15:34 +0700

    Add file word.java

--- Word.java
+++ Word.java
@@ -1,5 +1,30 @@
 package community.com;
 
 public class Word {
-
+	private String word_target;//từ mới 
+	private String word_explain;//giải nghĩa
+	public Word(String word_target , String word_expain)
+	{
+		//constructor truyền đối số
+		this.word_target =word_target;
+		this.word_explain = word_explain;
+	}
+	// phương thức set ,get 
+	public void setWord_Target (String word_target)
+	{
+		this.word_target =this.word_target;
+	}
+	public void setWord_Explain(String word_explain )
+	{
+		this.word_explain =this.word_explain;
+	}
+	public String getWord_Target()
+	{
+		return this.word_target;
+	}
+	public String getWord_Explain()
+	{
+		return this.word_explain;
+	}
+	
 }
