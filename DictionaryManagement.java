bcef865b2dbe6db0715ba2139a0568f65a6ce3e3
NguyenKhanhIE3 <lord251200@gmail.com>
2018-10-02 08:51:01 +0700

    add file

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,12 +1,13 @@
 package community.com;
 
+
 import java.util.Scanner;
 
 public class DictionaryManagement {
 	public static void insertFromCommandline(Dictionary dtn)
 	{
 		Scanner sc = new Scanner (System.in);
-		int numberWord = Interger.parse (sc.nextLine());
+		int numberWord = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i<numberWord ;i++)
 		{
 			String word_target = sc.nextLine();
