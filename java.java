b1925981a7589059b230e71f01eabc573aa549dc
trinhnoo <37481788+trinhnoo@users.noreply.github.com>
2018-10-21 02:12:48 +0700

    Add files via upload

---
+++ java.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package folder;
+
+/**
+ *
+ * @author PHI LONG
+ */
+public class java {
+
+    public java() {
+    }
+
+    static class awt {
+
+        public awt() {
+        }
+
+        static class Label {
+
+            public Label() {
+            }
+        }
+    }
+    
+}
